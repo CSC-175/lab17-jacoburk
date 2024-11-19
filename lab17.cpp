b94dadeee0051ec778b3b244c5b8e2212acfdb8e
Jacob Burke <jacoburk@umich.edu>
2024-11-19 15:27:57 -0500

    Attempt 2

--- lab17.cpp
+++ lab17.cpp
@@ -1,11 +1,11 @@
 // Definition of findHigh function goes here
 template <typename A , typename B , typename C>
-void findHigh(A arr[], B size, int &ind, C &val ) {
-    val = arr[0];
+void findHigh(A arr[], B size, int &index,C &high ) {
+    high = arr[0];
     for (int i = 0; i < size; i++) {
-        if (val < arr[i]) {
-            ind =1;
-            val = arr[i];
+        if (high < arr[i]) {
+            index =1;
+            high = arr[i];
         }
     }
 
